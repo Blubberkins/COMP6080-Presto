015d2043f85d9b3aa0a29ec93fdd1c9a21285179
Edward <eddieboyemo@gmail.com>
2024-04-19 21:46:34 +1000

    edits

--- frontend/src/happy_path.js
+++ frontend/src/happy_path.js
@@ -6,17 +6,6 @@ context('Admin Happy Path', () => {
   it('Successfully logs in after entering the correct credentials', () => {
     // Log in with the correct credentials
     const email = 'soorria@email.com';
-    const password = 'super secure password';
-    const welcomeText = "You're logged in!";
+    const password = 'ThisIsAPassword';
 
-
-    cy.get('input[name=email]').focus().type(email);
-    cy.get('input[name=password]').focus().type(password);
-    cy.get('button[type=submit]').click();
-
-    // Check that it is successful
-    cy.get('h1').then((h1) => {
-      expect(h1.text()).to.contain(welcomeText);
-    });
-  });
 });
