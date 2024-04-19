7bdffac460c6582379d285055710a6766a7d9bca
Edward <eddieboyemo@gmail.com>
2024-04-19 13:54:22 +1000

    Modal Stub

---
+++ frontend/src/components/CreatePresentationModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useStyles } from '../styles';
+
+const CreatePresentationModal = () => {
+    const classes = useStyles();
+
+};
+
+export default CreatePresentationModal;
